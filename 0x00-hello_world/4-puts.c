2d53b662f984a42a6ac80c0b2ba014d17982a3c0
adeyemialameen04 <adeyemialameen04@gmail.com>
2023-12-17 22:46:52 +0100

    puts done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/* C program to print "Programming is like building a multilingual puzzle" */
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It prints a multilingual puzzle message using the puts function and
+ * returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "Programming is like building a multilingual puzzle";
+puts(message);
+return (0);
+}
