4c4c2683b62658c11cb48f8db11475521ece7348
adeyemialameen04 <adeyemialameen04@gmail.com>
2023-12-17 22:52:30 +0100

    5-printf.c done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It prints a multilingual puzzle message using the puts function and
+ * returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "with proper grammar, but the outcome is a piece of art,";
+puts(message);
+return (0);
+}
+
