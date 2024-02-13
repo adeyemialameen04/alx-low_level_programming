0cd973f8240c9f65b57a3d830b45e05958629cc3
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-02-13 20:09:38 +0100

    fixed task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -15,10 +16,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int len = strlen(text_content);
 	int written, fd;
+	mode_t file_permissions = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0660);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, file_permissions);
 
 	if (fd == -1)
 		return (-1);
