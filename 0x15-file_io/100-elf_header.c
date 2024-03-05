1495bed2544ba9f3f480fbde4c6b2dbcd03dd1bb
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 23:00:50 +0100

    trying to fix check errors :sob

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include <elf.h>
 
 /**
@@ -15,7 +16,6 @@ int main(int argc, char *argv[])
 {
 	int fd, i;
 	Elf32_Ehdr ehdr32;
-	Elf64_Ehdr ehdr64;
 
 	if (argc != 2)
 	{
@@ -30,68 +30,68 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
+	/*Read the ELF header*/
 	if (read(fd, &ehdr32, sizeof(ehdr32)) == sizeof(ehdr32))
 	{
-		printf("ELF Header:\n");
-		printf("  Magic:   ");
-		for (i = 0; i < EI_NIDENT; i++)
-			printf("%02x%c", ehdr32.e_ident[i], i + 1 < EI_NIDENT ? ' ' : '\n');
-
-		printf("  Class:                             ELF32\n");
-		printf("  Data:                              2's complement, ");
-		printf("little endian\n");
-		printf("  Version:                           %d (current)\n",
-			   ehdr32.e_ident[EI_VERSION]);
-
-		printf("  OS/ABI:                            ");
-		switch (ehdr32.e_ident[EI_OSABI])
+		/*Check if the file is a valid ELF binary*/
+		if (memcmp(ehdr32.e_ident, ELFMAG, SELFMAG) == 0)
 		{
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD\n");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("UNIX - Solaris\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", ehdr32.e_ident[EI_OSABI]);
+			printf("ELF Header:\n");
+			printf("  Magic:   ");
+			for (i = 0; i < EI_NIDENT; i++)
+				printf("%02x%c", ehdr32.e_ident[i], i + 1 < EI_NIDENT ? ' ' : '\n');
+
+			/*Print other ELF header information*/
+			printf("  Class:                             ELF32\n");
+			printf("  Data:                              2's complement, ");
+			printf("little endian\n");
+			printf("  Version:                           %d (current)\n",
+				   ehdr32.e_ident[EI_VERSION]);
+
+			printf("  OS/ABI:                            ");
+			switch (ehdr32.e_ident[EI_OSABI])
+			{
+			case ELFOSABI_SYSV:
+				printf("UNIX - System V\n");
+				break;
+			case ELFOSABI_NETBSD:
+				printf("UNIX - NetBSD\n");
+				break;
+			case ELFOSABI_SOLARIS:
+				printf("UNIX - Solaris\n");
+				break;
+			default:
+				printf("<unknown: %x>\n", ehdr32.e_ident[EI_OSABI]);
+			}
+
+			printf("  ABI Version:                       %d\n",
+				   ehdr32.e_ident[EI_ABIVERSION]);
+
+			printf("  Type:                              ");
+			switch (ehdr32.e_type)
+			{
+			case ET_NONE:
+				printf("NONE (Unknown type)\n");
+				break;
+			case ET_EXEC:
+				printf("EXEC (Executable file)\n");
+				break;
+			case ET_DYN:
+				printf("DYN (Shared object file)\n");
+				break;
+			default:
+				printf("<unknown: %x>\n", ehdr32.e_type);
+			}
+
+			printf("  Entry point address:               %#lx\n",
+				   (unsigned long)ehdr32.e_entry);
 		}
-
-		printf("  ABI Version:                       %d\n",
-			   ehdr32.e_ident[EI_ABIVERSION]);
-
-		printf("  Type:                              ");
-		switch (ehdr32.e_type)
+		else
 		{
-		case ET_NONE:
-			printf("NONE (Unknown type)\n");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)\n");
-			break;
-		case ET_EXEC:
-			printf("EXEC (Executable file)\n");
-			break;
-		case ET_DYN:
-			printf("DYN (Shared object file)\n");
-			break;
-		case ET_CORE:
-			printf("CORE (Core file)\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", ehdr32.e_type);
+			fprintf(stderr, "%s is not a valid ELF binary.\n", argv[1]);
+			close(fd);
+			return (98);
 		}
-
-		printf("  Entry point address:               %#lx\n",
-			   (unsigned long)ehdr32.e_entry);
-		}
-
-	else if (read(fd, &ehdr64, sizeof(ehdr64)) == sizeof(ehdr64))
-	{
-
-		printf("ELF Header: (Handle ELF64 here)\n");
 	}
 	else
 	{
