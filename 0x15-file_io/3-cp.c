7f6072b89b168f2d234f17e95d91f27c8bba4fda
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 21:21:52 +0100

    trying to fix 3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,32 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- *	main - copies the content of a file to another
- *	@argc: number of arguments passed to the program
- *	@argv: array of arguments passed to the program
- *	Return: 0 on success, exit status otherwise
- *	Description:
- *	This function copies the contents of a file specified by the first
- *	argument (argv[1]) to another file specified by the second argument
- *	(argv[2]). It opens both files, reads from the source file in chunks
- *	of BUFFER_SIZE, and writes those chunks to the destination file until
- *	the end of the source file is reached. Error messages are printed to
- *	standard error using dprintf in case of any errors during opening,
- *	reading, or writing. File descriptors are closed properly upon success
- *	or error.
- *	Exit status:
- *	97: If incorrect number of arguments are provided
- *	98: If there is an error opening the source file
- *	99: If there is an error reading from the source file or writing to
- *	  the destination file
- *	100: If there is an error closing either file descriptor
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of arguments passed to the program.
+ *
+ * Return: 0 on success, exit status otherwise.
  */
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[])
@@ -52,12 +38,11 @@ int main(int argc, char *argv[])
 	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(to);
+		close(from);
 		exit(99);
 	}
 
-	do
-	{
+	do {
 		nread = read(from, buf, BUFFER_SIZE);
 		if (nread == -1)
 		{
