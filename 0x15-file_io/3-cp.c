2d3212cfbe71f3806e7fcce4e5c99ab8c5add223
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 21:25:25 +0100

    fixed cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close(from);
 			close(to);
-			exit(99);
+			exit(98);
 		}
 		if (nread == 0)
 		{
