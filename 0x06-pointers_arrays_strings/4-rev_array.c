301335cedbad262c7bd35e511c1140bc249cbdb8
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 16:18:31 +0100

    fixing betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,17 +12,17 @@
  */
 void reverse_array(int *a, int n)
 {
-    int start = 0;
-    int end = n - 1;
+int start = 0;
+int end = n - 1;
 
-    while (start < end)
-    {
-        int temp = a[start];
+while (start < end)
+{
+int temp = a[start];
 
-        a[start] = a[end];
-        a[end] = temp;
+a[start] = a[end];
+a[end] = temp;
 
-        start++;
-        end--;
-    }
+start++;
+end--;
+}
 }
