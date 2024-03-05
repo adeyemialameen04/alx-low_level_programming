cb51cd9d5da21b9182897fc63b11db90dbcb7bb2
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 21:28:16 +0100

    trying to fix append errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,16 +15,17 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len = strlen(text_content), written;
+	int fd, len = (text_content != NULL) ? strlen(text_content) : 0;
+	ssize_t written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (len > 0)
 	{
 		written = write(fd, text_content, len);
 		if (written == -1)
