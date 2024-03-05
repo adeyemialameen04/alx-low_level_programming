9ea9146716de9e015ee70827b2222e415af789ad
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 22:35:42 +0100

    testing

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -68,13 +68,18 @@ int main(int argc, char *argv[])
 		case ET_NONE:
 			printf("NONE (Unknown type)\n");
 			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
 		case ET_EXEC:
 			printf("EXEC (Executable file)\n");
 			break;
 		case ET_DYN:
 			printf("DYN (Shared object file)\n");
 			break;
-
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
 		default:
 			printf("<unknown: %x>\n", ehdr32.e_type);
 		}
