9c3b1172cf2a8a5001cc57ce2afbdfb630a86243
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 21:52:10 +0100

    yepa yepo

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,22 +5,45 @@
 #include <elf.h>
 
 /**
- * print_elf_header - Print information from the ELF header.
- * @ehdr: Pointer to the ELF header structure.
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * Return: 0 on success, 98 on failure.
  */
-void print_elf_header(Elf64_Ehdr *ehdr);
-void print_elf_header(Elf64_Ehdr *ehdr)
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
-	int i;
+	int fd;
+	Elf64_Ehdr ehdr;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error");
+		return (98);
+	}
+
+	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+	{
+		perror("Error");
+		close(fd);
+		return (98);
+	}
 
 	printf("ELF Header:\n");
 
 	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-		printf("%02x%c", ehdr->e_ident[i], i + 1 < EI_NIDENT ? ' ' : '\n');
+	for (int i = 0; i < EI_NIDENT; i++)
+		printf("%02x%c", ehdr.e_ident[i], i + 1 < EI_NIDENT ? ' ' : '\n');
 
 	printf("  Class:                             ");
-	switch (ehdr->e_ident[EI_CLASS])
+	switch (ehdr.e_ident[EI_CLASS])
 	{
 	case ELFCLASS32:
 		printf("ELF32\n");
@@ -29,11 +52,11 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 		printf("ELF64\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", ehdr->e_ident[EI_CLASS]);
+		printf("<unknown: %x>\n", ehdr.e_ident[EI_CLASS]);
 	}
 
 	printf("  Data:                              ");
-	switch (ehdr->e_ident[EI_DATA])
+	switch (ehdr.e_ident[EI_DATA])
 	{
 	case ELFDATA2LSB:
 		printf("2's complement, little endian\n");
@@ -42,14 +65,14 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 		printf("2's complement, big endian\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", ehdr->e_ident[EI_DATA]);
+		printf("<unknown: %x>\n", ehdr.e_ident[EI_DATA]);
 	}
 
 	printf("  Version:                           %d (current)\n",
-		   ehdr->e_ident[EI_VERSION]);
+		   ehdr.e_ident[EI_VERSION]);
 
 	printf("  OS/ABI:                            ");
-	switch (ehdr->e_ident[EI_OSABI])
+	switch (ehdr.e_ident[EI_OSABI])
 	{
 	case ELFOSABI_SYSV:
 		printf("UNIX - System V\n");
@@ -59,14 +82,14 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 		break;
 
 	default:
-		printf("<unknown: %x>\n", ehdr->e_ident[EI_OSABI]);
+		printf("<unknown: %x>\n", ehdr.e_ident[EI_OSABI]);
 	}
 
 	printf("  ABI Version:                       %d\n",
-		   ehdr->e_ident[EI_ABIVERSION]);
+		   ehdr.e_ident[EI_ABIVERSION]);
 
 	printf("  Type:                              ");
-	switch (ehdr->e_type)
+	switch (ehdr.e_type)
 	{
 	case ET_NONE:
 		printf("NONE (Unknown type)\n");
@@ -79,9 +102,12 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 		break;
 
 	default:
-		printf("<unknown: %x>\n", ehdr->e_type);
+		printf("<unknown: %x>\n", ehdr.e_type);
 	}
 
 	printf("  Entry point address:               %#lx\n",
-		   (unsigned long)ehdr->e_entry);
+		   (unsigned long)ehdr.e_entry);
+
+	close(fd);
+	return (0);
 }
