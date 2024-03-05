82d9454c6ab33b0a1fddbee5ba1151fc27907120
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 21:31:24 +0100

    fixed create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,20 +14,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int len = strlen(text_content);
 	int written, fd;
 	mode_t file_permissions = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, file_permissions);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		written = write(fd, text_content, len);
+		written = write(fd, text_content, strlen(text_content));
 		if (written == -1)
 		{
 			close(fd);
