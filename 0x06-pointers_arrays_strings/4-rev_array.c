df232a01f038cc3704a39474af7e6422e14c5336
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-05 16:13:53 +0100

    fixed rev

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,12 +17,11 @@ void reverse_array(int *a, int n)
 
     while (start < end)
     {
-        // Swap elements at start and end indices
         int temp = a[start];
+
         a[start] = a[end];
         a[end] = temp;
 
-        // Move indices towards each other
         start++;
         end--;
     }
