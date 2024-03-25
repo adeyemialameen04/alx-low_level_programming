7fad7e653d59f89312665d46245c1d1a90d4ac58
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-25 14:13:03 +0100

    ddd

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -2,24 +2,24 @@
 #include <string.h>
 
 /**
-* _strcat - Concatenates two strings.
-* @dest: The destination string.
-* @src: The source string.
-*
-* Return: A pointer to the resulting concatenated string.
-*/
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting concatenated string.
+ */
 char *_strcat(char *dest, char *src)
 {
-int len1 = 0;
-int len2 = 0, i;
+	int len1 = 0;
+	int len2 = 0, i;
 
-len1 = strlen(dest);
-len2 = strlen(src);
+	len1 = strlen(dest);
+	len2 = strlen(src);
 
-for (i = 0; i <= len2; i++)
-{
-dest[len1 + i] = src[i];
-}
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
 
-return dest;
+	return (dest);
 }
