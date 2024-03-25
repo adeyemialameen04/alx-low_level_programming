f384a16cd63802fb3f8753e100967405d1c63a79
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-25 14:11:50 +0100

    cpython

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,55 @@
+
+/**
+ * add - Function to add two numbers.
+ * @a: First interget.
+ * @b: Second integer.
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Function to subtract two numbers.
+ * @a: First interget.
+ * @b: Second integer.
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Function to multiply two numbers.
+ * @a: First interget.
+ * @b: Second integer.
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Function to divide two numbers.
+ * @a: First interget.
+ * @b: Second integer.
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Function to find the modulo of two numbers.
+ * @a: First interget.
+ * @b: Second integer.
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
