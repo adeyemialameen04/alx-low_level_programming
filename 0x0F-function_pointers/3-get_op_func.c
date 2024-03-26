aa75170325ed9008ba7d5836b7dbf0f599d54522
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-26 22:29:43 +0100

    just pushing

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,30 +2,29 @@
 #include "3-calc.h"
 
 /**
-* get_op_func - Selects the correct function to perform the operation.
-* @s: Operator passed as an argument to the program.
-*
-* Return: Pointer to the function that corresponds to the operator,
-*         or NULL if the operator is not one of the expected operators.
-*/
+ * get_op_func - Selects the correct function to perform the operation.
+ * @s: Operator passed as an argument to the program.
+ *
+ * Return: Pointer to the function that corresponds to the operator,
+ *         or NULL if the operator is not one of the expected operators.
+ */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i = 0;
 
-while (ops[i].op != NULL)
-{
-if (*(ops[i].op) == *s && *(s + 1) == '\0')
-return (ops[i].f);
-i++;
-}
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
-return (NULL);
+	return (NULL);
 }
