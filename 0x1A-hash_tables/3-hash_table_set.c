eff236de086a9f268d3df1fe62a7844255332dc5
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-13 02:28:37 +0100

    implemnted the set function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,7 +8,7 @@
  */
 hash_node_t *create_hash_node(const char *key, const char *value)
 {
-	hash_node_t *item = malloc(sizeof(hash_node_t *) * 1);
+	hash_node_t *item = malloc(sizeof(hash_node_t));
 
 	if (item == NULL)
 	{
@@ -43,40 +43,30 @@ hash_node_t *create_hash_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *tmp;
+	hash_node_t *hash_node, *curr_hash_node;
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-	/* Check for valid input */
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 
-	/* Get the index for the key */
-	index = key_index((const unsigned char *)key, ht->size);
-
-	/* Check if the key already exists */
-	tmp = ht->array[index];
-	while (tmp != NULL)
+	curr_hash_node = ht->array[index];
+	while (curr_hash_node != NULL)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		if (strcmp(curr_hash_node->key, key) == 0)
 		{
-			/* Update the existing value */
-			free(tmp->value);
-			tmp->value = strdup(value);
+			free(curr_hash_node->value);
+			curr_hash_node->value = strdup(value);
 			return (1);
 		}
-		tmp = tmp->next;
+
+		curr_hash_node = curr_hash_node->next;
 	}
 
-	/* Create a new node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	hash_node = create_hash_node(key, value);
+	if (hash_node == NULL)
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	/* Add the new node to the beginning of the linked list */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
 
 	return (1);
 }
