3f53b3503ab763efe04277e31c12132881213826
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-13 03:14:16 +0100

    fixed for collisions

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,8 +17,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	hash_node = ht->array[index];
 
-	if (hash_node == NULL)
-		return (NULL);
+	while (hash_node != NULL)
+	{
+		if (strcmp(hash_node->key, key) == 0)
+			return (hash_node->value);
 
-	return (hash_node->value);
+		hash_node = hash_node->next;
+	}
+
+	return (NULL);
 }
