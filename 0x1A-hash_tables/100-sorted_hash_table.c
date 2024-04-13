0b77aff108ea3977a8199a051c14602dcfe01684
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-13 20:06:33 +0100

    you?

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -39,13 +39,15 @@ shash_table_t *shash_table_create(unsigned long int size)
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-	shash_node_t *new_node, *curr;
+	shash_node_t *new_node, *curr, *prev = NULL;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+
+	/*Check if key already exists*/
 	curr = ht->array[index];
 	while (curr != NULL)
 	{
@@ -58,47 +60,44 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		curr = curr->next;
 	}
 
+	/*Create a new node*/
 	new_node = create_s_hash_node(key, value);
 	if (new_node == NULL)
 		return (0);
+
+	/*Insert into the hash table array*/
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	if (ht->shead == NULL)
+	/*Insert into the sorted linked list*/
+	curr = ht->shead;
+	while (curr != NULL && strcmp(curr->key, key) < 0)
 	{
+		prev = curr;
+		curr = curr->snext;
+	}
+
+	if (prev == NULL)
+	{
+		/*Insert at the beginning of the list*/
+		new_node->snext = ht->shead;
+		if (ht->shead != NULL)
+			ht->shead->sprev = new_node;
 		ht->shead = new_node;
-		ht->stail = new_node;
-		new_node->sprev = NULL;
-		new_node->snext = NULL;
 	}
 	else
 	{
-		curr = ht->shead;
-		while (curr != NULL && strcmp(curr->key, key) < 0)
-			curr = curr->next;
-
-		if (curr == NULL)
-		{
-			new_node->sprev = ht->stail;
-			ht->stail->snext = new_node;
-			ht->stail = new_node;
-			new_node->snext = NULL;
-		}
-		else if (curr == ht->shead)
-		{
-			new_node->next = ht->shead;
-			ht->shead->sprev = new_node;
-			ht->shead = new_node;
-			new_node->sprev = NULL;
-		}
-		else
-		{
-			new_node->sprev = curr->sprev;
-			new_node->snext = curr;
-			curr->sprev->snext = new_node;
-			curr->sprev = new_node;
-		}
+		/*Insert after 'prev'*/
+		new_node->sprev = prev;
+		new_node->snext = prev->snext;
+		if (prev->snext != NULL)
+			prev->snext->sprev = new_node;
+		prev->snext = new_node;
 	}
+
+	/*Update tail if necessary*/
+	if (new_node->snext == NULL)
+		ht->stail = new_node;
 
 	return (1);
 }
@@ -143,13 +142,11 @@ void shash_table_print(const shash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-	curr = ht->shead;
 	printf("{");
-	while (curr != NULL)
+	for (curr = ht->shead; curr != NULL; curr = curr->snext)
 	{
 		printf("'%s': '%s'", curr->key, curr->value);
-		curr = curr->next;
-		if (curr != NULL)
+		if (curr->snext != NULL)
 		{
 			printf(", ");
 		}
