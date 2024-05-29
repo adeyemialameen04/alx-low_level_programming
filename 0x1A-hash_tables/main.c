a8b929f0e2261e459a40eae3d7e0e360c0a26b87
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-05-29 12:22:28 +0100

    makefiles

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
@@ -11,24 +10,20 @@
 int main(void)
 {
 	shash_table_t *ht;
+	shash_node_t *curr;
 
 	ht = shash_table_create(1024);
 	shash_table_set(ht, "y", "0");
-	shash_table_print(ht);
 	shash_table_set(ht, "j", "1");
-	shash_table_print(ht);
 	shash_table_set(ht, "c", "2");
-	shash_table_print(ht);
 	shash_table_set(ht, "b", "3");
-	shash_table_print(ht);
 	shash_table_set(ht, "z", "4");
-	shash_table_print(ht);
 	shash_table_set(ht, "n", "5");
-	shash_table_print(ht);
 	shash_table_set(ht, "a", "6");
-	shash_table_print(ht);
 	shash_table_set(ht, "m", "7");
+
+	curr = ht->array[0];
+	printf("%s", curr->key);
 	shash_table_print(ht);
-	shash_table_print_rev(ht);
 	return (EXIT_SUCCESS);
 }
